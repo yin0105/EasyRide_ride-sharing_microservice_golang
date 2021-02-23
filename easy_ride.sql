e302a74e8843d02357670dce48c5d3fe2b571907
solutionLi <momo821123@outlook.com>
2021-02-23 17:18:45 +0900

    Add easy_ride.sql

---
+++ easy_ride.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Professional v12.09 (64 bit)
+MySQL - 10.4.11-MariaDB : Database - easy_ride
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`easy_ride` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `easy_ride`;
+
+/*Table structure for table `roster` */
+
+DROP TABLE IF EXISTS `roster`;
+
+CREATE TABLE `roster` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(20) NOT NULL,
+  `pwd` varchar(20) NOT NULL,
+  `rate` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `roster` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
